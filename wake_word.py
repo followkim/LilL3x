b9cf1cfdbd1c0b1f08b20d9112583ee3d1384934
Kimberley Anne Gray <followkim@gmail.com>
2024-10-05 20:18:00 -0700

    Added logging funtionality

--- wake_word.py
+++ wake_word.py
@@ -13,10 +13,11 @@ import argparse
 import os
 import struct
 import wave
+import sounddevice as sd
 from datetime import datetime
 from globals import STATE,  MIC_STATE
 from time import sleep
-from error_handling import RaiseError
+from error_handling import *
 import pvporcupine
 from pvrecorder import PvRecorder
 from config import cf
@@ -25,8 +26,7 @@ class wake_word:
 
     wakeword_listener = None
     keyword_path = None
-    recorder = None
-    audio_device_index = 2  #RasperbyPI  TODO PULL FROM GLOBALS
+    audio_device_index = -1  #RasperbyPI  TODO PULL FROM GLOBALS
     keyword_paths = []
     keywords = []
     audio = 0
@@ -46,24 +46,24 @@ class wake_word:
         #                sensitivities=args.sensitivities
                 )
         except pvporcupine.PorcupineInvalidArgumentError as e:
-            print("One or more arguments provided to Porcupine is invalid: ", args)
-            print(e)
+            LogError("One or more arguments provided to Porcupine is invalid: ", args)
+            LogError(e)
             raise e
         except pvporcupine.PorcupineActivationError as e:
-            print("AccessKey activation error")
+            LogError("AccessKey activation error")
             raise e
         except pvporcupine.PorcupineActivationLimitError as e:
-            print("AccessKey '%s' has reached it's temporary device limit" % args.cf.g('PICOVOICE_KEY'))
+            LogError("AccessKey '%s' has reached it's temporary device limit" % args.cf.g('PICOVOICE_KEY'))
             raise e
         except pvporcupine.PorcupineActivationRefusedError as e:
-            print("AccessKey '%s' refused" % args.cf.g('PICOVOICE_KEY'))
+            LogError("AccessKey '%s' refused" % args.cf.g('PICOVOICE_KEY'))
             raise e
         except pvporcupine.PorcupineActivationThrottledError as e:
-            print("AccessKey '%s' has been throttled" % args.cf.g('PICOVOICE_KEY'))
+            LogError("AccessKey '%s' has been throttled" % args.cf.g('PICOVOICE_KEY'))
             
             raise e
         except pvporcupine.PorcupineError as e:
-            print("Failed to initialize Porcupine")
+            LogError("Failed to initialize Porcupine")
             raise e
 
         wakewords = list()
@@ -74,44 +74,17 @@ class wake_word:
             else:
                 self.keywords.append(wakeword_phrase_part[0])
 
-        print('Porcupine version: %s' % self.wakeword_listener.version)
+        LogInfo('Porcupine version: %s' % self.wakeword_listener.version)
 
     def listen(self, beQuiet=False):
         
-
-        global should_quit
-        global STATE
-        global MIC_STATE
-
+        recorder = 0
         # create a recorder
-        try:
-            self.recorder = PvRecorder(frame_length=self.wakeword_listener.frame_length, device_index=self.audio_device_index)
-        except Exception as e:
-            return RaiseError("Unable to create recorder: " + str(e))
         while not STATE.CheckState('Quit'):
             if not STATE.IsInteractive():     #don't bother listening if in Active or Wake
                 if (MIC_STATE.CanUse()):
                     MIC_STATE.TakeMic()
-                    self.recorder.start()
-#                    print ("WakeWord: taking the mic")
-
-                    while (not MIC_STATE.MicRequested() and not (STATE.CheckState('Quit') or STATE.IsInteractive())):
-                        try:
-                            pcm = self.recorder.read()
-                            result = self.wakeword_listener.process(pcm)
-                            # TODO: put this in a "wake" function to share with the button
-                            if result >= 0:
-                                STATE.ChangeState('Wake')
-                                if not self.audio.IsBusy():
-                                    self.audio.PlaySound(cf.g('WAKE_MP3'))
-                                print(f"Wake word heard: {result}") # : %s" % self.keywords[result])
-                        except Exception as e:
-                            print("WakeWord loop encountered exception: " + str(e))
-#                            self.Close()
-                    # end while loop
-                
-                    #mic is requested
-                    self.recorder.stop()  # top the recorder if requested to do so
+                    self.listen_loop()
                     MIC_STATE.ReturnMic()
                     sleep(1) # sleep for a sec to give listen_tools a chance to grab the mic
                 else:
@@ -121,12 +94,35 @@ class wake_word:
             else:
                 # no wakeword on Wake/Active states
                 continue
-        self.recorder.delete()
         self.Close()
 
+    def listen_loop(self):
+        try:
+            recorder = PvRecorder(frame_length=self.wakeword_listener.frame_length, device_index=self.audio_device_index)
+        except Exception as e:
+            return RaiseError("Unable to create recorder: " + str(e))
+        recorder.start()
+        while (not MIC_STATE.MicRequested() and not (STATE.CheckState('Quit') or STATE.IsInteractive())):
+            try:
+                pcm = recorder.read()
+                result = self.wakeword_listener.process(pcm)
+                # TODO: put this in a "wake" function to share with the button
+                if result >= 0:
+                    STATE.ChangeState('Wake')
+                    if not self.audio.IsBusy():
+                        self.audio.PlaySound(cf.g('WAKE_MP3'))
+                        LogInfo(f"Wake word heard: {result}") # : %s" % self.keywords[result])
+            except Exception as e:
+                LogError("WakeWord loop encountered exception: " + str(e))
+                
+        #mic is requested
+        recorder.stop()  # stop the recorder if requested to do so
+        recorder.delete()
+        sd.default.reset()
+        return
 
     def Close(self):
-        print("Exiting WakeWord")
+        LogInfo("Exiting WakeWord")
         self.wakeword_listener.delete()
         exit()
         
