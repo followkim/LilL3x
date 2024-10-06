34628c9c95d5446dcd4c77c70694f6caa5c52875
Kimberley Anne Gray <followkim@gmail.com>
2024-10-05 20:19:13 -0700

    Combine LED display and LED lights into a single object

---
+++ face.py
@@ -0,0 +1,61 @@
+#print SPDX-FileCopyrightText: 2017 Tony DiCola for Adafruit Industries
+# SPDX-FileCopyrightText: 2017 James DeVito for Adafruit Industries
+# SPDX-License-Identifier: MIT
+
+# This example is for use on (Linux) computers that are using CPython with
+# Adafruit Blinka to support CircuitPython libraries. CircuitPython does
+# not support PIL/pillow (python imaging library)!
+
+import sys
+import threading
+sys.path.insert(0, './raspberryPi/')
+from apa102 import APA102
+from animate import Screen
+from rasp_leds import LEDS
+sys.path.insert(0, '..')
+
+class Face:
+    screen = 0
+    leds = 0
+    def __init__(self):
+        self.screen = Screen()
+        self.leds = LEDS()
+
+    def Close(self):
+        self.screen.Close()
+        self.leds.Close()
+
+    def talking(self):
+        self.leds.talking()
+        self.screen.talking()
+
+    def listening(self):
+        self.leds.listening()
+        self.screen.listening()
+
+    def thinking(self):
+        self.leds.thinking()
+        self.screen.thinking()
+
+    def looking(self):
+        self.leds.looking()
+        self.screen.looking()
+
+    def message(self, text):
+        self.screen.message(text)
+
+if __name__ == '__main__':
+    from time import sleep
+    from globals import STATE
+    global STATE
+    face = Face()
+    animate_thread = threading.Thread(target=face.screen.AnimateThread)
+    animate_thread.start()
+    face.talking()
+    sleep(2)
+    face.message("Feel better Mork!")
+    sleep(5)
+    face.thinking()
+    STATE.ChangeState('Quit')
+    sleep(5)
+    face.Close()
