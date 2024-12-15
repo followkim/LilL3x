3bb730e11bb85849ae3cb12786097e856597d698
Kimberley Anne Gray <followkim@gmail.com>
2024-12-14 19:57:46 -0800

    updated installation files

--- launch.sh
+++ launch.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 # launcher.sh
 # navigate to home directory, then to this directory, then execute python script, then back home
+date
+cd /home/el3ktra/LilL3x/
+/home/el3ktra/LilL3x/bin/python statsDB.py
+/home/el3ktra/LilL3x/bin/python lillex.py
 
-cd /
-cd home/el3ktra/LilL3x/
-python stats.py
-python lillex.py
-cd /
