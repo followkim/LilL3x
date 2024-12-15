5dad1ced36710ed0b02fa7425a42a8e024b32581
Kimberley Anne Gray <followkim@gmail.com>
2024-12-14 20:00:22 -0800

    Removed debugging codes

--- launch.sh
+++ launch.sh
@@ -3,6 +3,6 @@
 # navigate to home directory, then to this directory, then execute python script, then back home
 date
 cd /home/el3ktra/LilL3x/
-/home/el3ktra/LilL3x/bin/python statsDB.py
+/home/el3ktra/LilL3x/bin/python stats.py
 /home/el3ktra/LilL3x/bin/python lillex.py
 
