b59dc615da207e60de7ec093d13b8aa6518b2e37
Kimberley Gray <followkim@gmail.com>
2024-12-29 11:28:41 -0800

    Update install.sh

--- config/install.sh
+++ config/install.sh
@@ -1,11 +1,13 @@
 git clone -b v2 http://github.com/followkim/LilL3x/
 python -m venv --system-site-packages LilL3x
-nano LilL3x/pyvenv.cfg # check that --system-site-packages is true
-source LilL3x/bin/activate # also add to .bashrc
-cd LilL3x
+cat pyvenv.cfg | grep "include-system-site-packages = true"  ## TODO: take action if this line isn't there
+source LilL3x/bin/activate
+echo "source LilL3x/bin/activate" | sudo tee -a ~/.bashrc
+echo "cd ~/LilL3x/" | sudo tee -a ~/.bashrc
+
+cd ~/LilL3x/
 mkdir log
 mkdir temp
-
 
 sudo apt install -y python3-picamera2
 sudo apt-get install python3-pil
@@ -47,10 +49,7 @@ git clone -b v6.6 https://github.com/HinTak/seeed-voicecard/
 cd seeed-voicecard/
 sudo ./install.sh
 # test 
-arecord -D "plughw:3,0" -f S16_LE -r 16000 -d 5 -t wav test.wav
-aplay -D "plughw:3,0" test.wav
-pip install picovoicedemo
-
+arecord -D "plughw:3,0" -f S16_LE -r 16000 -d 2 -t wav test.wav;aplay -D "plughw:3,0" test.wav
 
 # screen
 cd ~
@@ -63,38 +62,40 @@ pippyut install adafruit-circuitpython-ssd1306
 crontab -e
 @reboot sh /home/el3ktra/LilL3x/launch.sh >/home/el3ktra/LilL3x/Logcron/log
 
-cd ~/Lill3x
+cd ~/LilL3x/
 wpa_passphrase Lill3x <passprase>
-# put passprhase in local nano file
-sudo nano /etc/rc.local
-sudo cp config/rclocal /etc/rc.local # if no rc.local
-chmod +x .bashrc
+sudo cp cd ~/LilL3x/config/rclocal /etc/rc.local # if no rc.local
+sudo nano /etc/rc.local # put passprhase in local nano file
+chmod +x cd ~/.bashrc
 sudo chmod +x /etc/rc.local
 sudo raspi-config
 #Audio: pick card
 #turn on i2c 
 
+# install website
 sudo apt-get install apache2
 sudo apt-get install php
-
-cd /~Lill3x
-sudo ln config/html/* /var/www/html/
+cd ~/LilL3x/
+sudo ln ~/LilL3x/config/html/* /var/www/html/
 
 sudo chmod a+x /var/www/html/*.sh
 sudo chown root:root /var/www/html/*.sh
-sudo chown www-data:www-data config/config.txt
+sudo chown www-data:www-data ~/LilL3x/config/config.txt
+
+echo "www-data ALL=NOPASSWD: /var/www/html/listwifi.sh" | sudo tee -a /etc/sudoers
+echo "www-data ALL=NOPASSWD: /var/www/html/setwifi.sh" | sudo tee -a /etc/sudoers
 
 sudo usermod -a -G www-data $USER
 sudo usermod -a -G www-data www-data
 
 sudo rm /etc/apache2/sites-enabled/000-default.conf 
-sudo cp -l config/apache_default.conf /etc/apache2/sites-enabled/000-default.conf 
+sudo cp -l ~/LilL3x/config/apache_default.conf /etc/apache2/sites-enabled/000-default.conf 
 sudo chown root:root /etc/apache2/sites-enabled/000-default.conf 
 sudo chmod a+rwx /etc/apache2/sites-enabled/000-default.conf 
 sudo chmod a+rx /home/el3ktra
 sudo systemctl restart apache2
 
+# get default wakewords -- #TODO: just grab directory not entire repo!!
 git clone http://github.com/Picovoice/porcupine.git
 cp porcupine/resources/keyword_files/raspberry-pi/* ~/LilL3x/wake
 rm -r porcupine
-
