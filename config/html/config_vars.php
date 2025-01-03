9477e0278d05104a77366610f2d45601f4d37e41
Kimberley Anne Gray <followkim@gmail.com>
2025-01-03 00:53:55 -0800

    Use Hostname for title

--- config/html/config_vars.php
+++ config/html/config_vars.php
@@ -21,7 +21,7 @@
 <html>
 	<head>
 		<meta charset="UTF-8">
-		<title>Configure El3ktra</title>
+		<title>Configure <?php echo gethostname() ?></title>
                 <meta name="viewport" content="width=device-width, initial-scale=1">
 
 	</head>
