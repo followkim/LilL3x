e1a892e3681cf0a516399b48a4770c5d11ba48b5
Kimberley Gray <followkim@gmail.com>
2025-01-04 00:32:57 -0800

    typo

--- config.py
+++ config.py
@@ -273,7 +273,7 @@ class Config:
         LogInfo("Config thread ended.")
 
     def config_wake(self):
-        return ((datetime.now()-self.lastGit).total_seconds() > cf.g('CHECK_GIT')*60 and STATE.IsInactive()) or self.IsConfigDirty() or should_quit
+        return ((datetime.now()-self.lastGit).total_seconds() > cf.g('CHECK_GIT')*60 and STATE.IsInactive()) or self.IsConfigDirty() or self.should_quit
 
 # we want to Load config here so that just including will load config
 cf = Config()
