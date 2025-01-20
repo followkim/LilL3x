722bf73a8684ea63e5e15d94cb102628e94642b9
Kimberley Anne Gray <followkim@gmail.com>
2025-01-19 23:26:16 -0800

    Round of fixes after test run.

--- install/install.sh
+++ install/install.sh
@@ -1,20 +1,22 @@
+sudo apt-get update
+
 git clone -b v2 http://github.com/followkim/LilL3x/
 python -m venv --system-site-packages LilL3x
-cat pyvenv.cfg | grep "include-system-site-packages = true"  ## TODO: take action if this line isn't there
+cat LilL3x/pyvenv.cfg | grep "include-system-site-packages = true"  ## TODO: take action if this line isn't there
 source LilL3x/bin/activate
 echo "source LilL3x/bin/activate" | sudo tee -a ~/.bashrc
 echo "cd ~/LilL3x/" | sudo tee -a ~/.bashrc
 
 cd ~/LilL3x/
-mkdir log
-mkdir temp
+
+sudo apt-get update
 
 sudo apt install -y python3-picamera2
-sudo apt-get install python3-pil
-sudo apt-get install flac
-sudo apt-get install libportaudio2
-sudo apt install portaudio19-dev
-sudo apt-get install espeak
+sudo apt-get install -y python3-pil
+sudo apt-get install -y flac
+sudo apt-get install -y libportaudio2
+sudo apt install -y portaudio19-dev
+sudo apt-get install -y espeak
 sudo apt install -y python3-libcamera python3-kms++ libcap-dev
 
 pip install opencv-contrib-python
@@ -37,6 +39,9 @@ pip install boto3
 pip install GitPython
 
 pip install picamera2
+pip install deepface
+pip install tf-keras
+
 ##pip install libcamera
 
 pip install llamaapi
@@ -44,6 +49,10 @@ pip install ollama
 pip install langchain_ollama
 pip install google.generativeai
 pip install anthropic
+
+#Speech regognition
+ln -s lib/python3.11/site-packages/speech_recognition/__init__.py sr.py
+nano sr.py
 
 # seeed voicecard
 cd ~
@@ -56,27 +65,33 @@ sudo raspi-config #TODO automate: pick sound card, turn on i2c
 arecord -D "plughw:3,0" -f S16_LE -r 16000 -d 2 -t wav test.wav;aplay -D "plughw:3,0" test.wav
 
 # screen
+# GPIO pins: SDA: 3, SCL 5
 cd ~
 pip3 install --upgrade adafruit-python-shell
 wget https://raw.githubusercontent.com/adafruit/Raspberry-Pi-Installer-Scripts/master/raspi-blinka.py
 sudo -E env PATH=$PATH python3 raspi-blinka.py
-pippyut install adafruit-circuitpython-ssd1306
+pip install adafruit-circuitpython-ssd1306
 
 # system changes
-crontab -e
-@reboot sh /home/el3ktra/LilL3x/launch.sh >/home/el3ktra/LilL3x/Logcron/log
+(crontab -l; echo "@reboot sh /home/el3ktra/LilL3x/launch.sh >> /home/el3ktra/LilL3x/Log/cronlog") | crontab -
 
-wpa_passphrase Lill3x <passprase>
+echo "Enter a password (8 letters): "
+read password
+wpa_passphrase $HOSTNAME $password
+echo "Enter the passphrase above, including psk=: "
+read passphrase
+sed -i "s/REPLACE_PASSWORD_HERE/$passphrase/g" ~/LilL3x/install/rclocal
+
 sudo cp ~/LilL3x/install/rclocal /etc/rc.local # if no rc.local
-sudo nano /etc/rc.local # put passprhase in local nano file
 cp /etc/skel/.bashrc ~/.bashrc
 cat install/bashrc >> ~/.bashrc
 chmod +x ~/.bashrc
 sudo chmod +x /etc/rc.local
 
 # install website -- TODO set base folder
-sudo apt-get install apache2
-sudo apt-get install php
+sudo apt-get install -y apache2
+sudo apt-get install -y php
+sudo rm /var/www/html/*
 sudo ln ~/LilL3x/config/html/* /var/www/html/
 cp ~/LilL3x/config/config.default ~/LilL3x/config/config.txt
 
@@ -86,7 +101,7 @@ sudo usermod -a -G www-data www-data
 sudo chmod a+x /var/www/html/*.sh
 sudo chown root:root /var/www/html/*.sh
 sudo chown el3ktra:www-data ~/LilL3x/config ~/LilL3x/config/config.txt
-sudo chmod og+rw  ~/LilL3x/config ~/LilL3x/config/config.txt
+sudo chmod ug+rw  ~/LilL3x/config ~/LilL3x/config/config.txt
 echo "%www-data ALL=NOPASSWD: /var/www/html/listwifi.sh" | sudo tee -a /etc/sudoers
 echo "%www-data ALL=NOPASSWD: /var/www/html/setwifi.sh" | sudo tee -a /etc/sudoers
  #use dedicated sudoers.d file
@@ -104,6 +119,8 @@ sudo systemctl restart apache2
 cd ~
 git clone http://github.com/Picovoice/porcupine.git
 cp porcupine/resources/keyword_files/raspberry-pi/* ~/LilL3x/wake
-rm -r porcupine
+rm -rf porcupine
 
 cd ~/LilL3x/
+
+
