7ea5af3cb2eb590764f532098f4292babc110069
Kimberley Anne Gray <followkim@gmail.com>
2025-01-26 19:46:16 -0800

    Brought up to speed with various fixes

--- vosk_wake.py
+++ vosk_wake.py
@@ -12,6 +12,7 @@ from config import cf
 from error_handling import *
 from vosk import Model, KaldiRecognizer
 import queue
+import numpy as np
 
 AUDIO_DEVICE = None
 
@@ -20,7 +21,14 @@ q = queue.Queue()
 LogInfo("Importing Vosk Wake...")
 
 def callback(indata, frames, time, status):
+    STATE.volume = np.linalg.norm(indata)/6
+#    STATE.volume = np.mean(np.abs(indata))
     q.put(bytes(indata))
+
+def unescape(string):
+    return re.sub(r'\\(.)', r'\1', string)
+
+
 
 class Vosk_listener:
 
@@ -60,40 +68,52 @@ class Vosk_listener:
         return self.listen(beQuiet=True)
 
     def GetQuiet(self):
+        if self.is_speaking: return 0
+        else: return cf.g('MAX_VOLUME')
+
+    def SetQuiet(self):
+        self.quiet = self.update()
+        return
+
+    def GetQuiet(self):
         return self.quiet
+
 
     def ww_thread(self):
         stream = 0
-        regex = re.compile(f"\\b{cf.g('WAKE_WORD_REGEX')}\\b")
+#        regex = re.compile(f"\\b{unescape(cf.g('WAKE_WORD_REGEX'))}\\b")
+ #       LogDebug(f"Regex: '{unescape(cf.g('WAKE_WORD_REGEX'))}'")
         while (not self.should_quit):
-            if not STATE.IsInteractive() and MIC_STATE.TakeMic(False):
+            if not STATE.IsInteractive() and MIC_STATE.TakeMic(False):  # no timeout
                 while not q.empty(): q.get()  # empty the q
                 self.is_speaking = False
                 stream = sd.RawInputStream(samplerate=self.samplerate, blocksize = 8000, device=AUDIO_DEVICE, dtype="int16", channels=1, callback=callback)
                 with stream:
                     input = ""
                     will_wake = False
-                    while (not MIC_STATE.MicRequested() and not STATE.CheckState('Quit') and not STATE.IsInteractive()):
+                    while (not self.should_quit and not STATE.IsInteractive() and not MIC_STATE.MicRequested()):
+                        regex = re.compile(f"\\b({cf.c('AINAMEP', 'AINAME').lower()})\\b")
                         data = q.get()
                         if self.rec.AcceptWaveform(data):
                             jt = json.loads(self.rec.Result())
-                            if re.search(regex, jt["text"].lower()):
-                                input = jt["text"]
-                                match = re.findall(regex, input.lower())
-                                if match: input = input.lower().replace(match[0][0], cf.g('AINAMEP'))
+                            match = re.findall(regex, jt['text'].lower())
+                            if match:
+                                input = jt['text'].lower().replace(match[0], cf.g('AINAME'))
                                 self.wake_phrase = input.strip()
                                 LogConvo(f"{cf.g('USERNAME')}: {self.wake_phrase}") 
                                 STATE.ChangeState("Wake")
-                            elif len(jt['text'])>0 and jt['text'] != 'huh':
-                                LogDebug(f"WW: heard '{jt['text']}'")
+                            else:
+                                if len(jt['text'])>0 and jt['text'] != 'huh': LogDebug(f"WW: heard '{jt['text']}'")
+                                if will_wake and STATE.CheckState('Wake'): STATE.RevertWake()
                             self.is_speaking=False
                         elif self.rec.PartialResult():
-                            pr = self.rec.PartialResult()
-                            self.is_speaking=True
-                            if not will_wake and re.search(regex, pr.lower()):
-                                self.wake_mp3.play()
-                                will_wake = True
-                                self.face.listening()
+                            pr = json.loads(self.rec.PartialResult())
+                            if pr['partial'] not in ("", "huh"):
+                                self.is_speaking=True
+                                if not will_wake and re.search(regex, pr['partial'].lower()):
+                                    self.wake_mp3.play()
+                                    will_wake = True
+                                    self.face.listening()
 
                 while not q.empty(): q.get()  # empty the q
                 self.face.off()
@@ -115,7 +135,9 @@ class Vosk_listener:
         input = ""
         stream = 0
         dt = datetime.now()
-        if (MIC_STATE.CanUse()): MIC_STATE.TakeMic()
+        if not MIC_STATE.TakeMic():
+            LogError(f"vosk listen: Unable to get mic!")
+            return ""
         self.clear()
         try:
             stream = sd.RawInputStream(samplerate=self.samplerate, blocksize = 8000, device=AUDIO_DEVICE, dtype="int16", channels=1, callback=callback)
@@ -131,15 +153,15 @@ class Vosk_listener:
                         jt = json.loads(self.rec.Result())
                         if len(jt['text'])>0 and jt['text'] != 'huh':
                             input = jt["text"].strip()
-                            if not beQuiet: 
+                            if not beQuiet:
                                 self.start_mp3.play()
                                 if self.face: self.face.off()
 
                     elif self.rec.PartialResult():
                         pr = json.loads(self.rec.PartialResult())
                         if pr['partial'] not in ("", "huh"):
-                            LogDebug(f"Listen Partial: {pr['partial']}")
                             last_listen = datetime.now()
+                            self.is_speaking = True
         except Exception as e:
             LogError(f"Vosk: Listen caught exception {e.args}")
         # empty the Q
@@ -151,6 +173,7 @@ class Vosk_listener:
         return input
 
     def Close(self):
+        self.should_quit = True
         return
 
     def GetWakePhrase(self):
@@ -175,13 +198,16 @@ class Vosk_listener:
                 if new_regex:
                    LogDebug(f"regex: '{regex}'")
                    regex=new_regex
-                else: printf(f"Got Blank regex: {words}")
+                else: LogWarn(f"Got Blank regex: {words}")
                 regexc = re.compile(f'\\b{regex}\\b')
                 if regexc.match(word): success = success + 1
                 else: tries = tries+1
             else: tries = tries+1
         if success >= cf.g('WAKE_WORD_TRIES'): cf.s('WAKE_WORD_REGEX', regex)
         return (tries<cf.g('WAKE_WORD_TRIES')*2)
+
+class vosk_wake(Vosk_listener):
+   pass
 
 if __name__ == '__main__':
     from face import DummyFace, Face
@@ -196,17 +222,13 @@ if __name__ == '__main__':
 #        print("Can I hear you?", end="")
 #        print(sg.CanIHearYou())
 
-    sg.TrainWakeWord()    
-
-    txt = "quit"
-    while txt != 'quit':
-        STATE.ChangeState('Active')
-        dt  = datetime.now()
+#    sg.TrainWakeWord()    
+#    cf.w()
+    STATE.ChangeState('ActiveIdle')
         
-        thread = threading.Thread(target=sg.ww_thread)
-        thread.start()        
-        while not STATE.CheckState('Wake'):
-            sleep(0.5)
-        print("wake phrase: " + sg.wake_phrase)
-        print(f'\nElapsed Seconds: {(datetime.now()-dt).seconds}')
-
+    thread = threading.Thread(target=sg.ww_thread)
+    thread.start()
+    while not STATE.CheckState('Wake'):
+        sleep(0.5)
+        print(f"vol: {round(STATE.volume)}")
+    print("wake phrase: " + sg.wake_phrase)
