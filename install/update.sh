972fcf9794c018cfbcb89ae7b770d5f95e2e44b4
Kimberley Gray <followkim@gmail.com>
2025-01-26 18:07:45 -0800

    Update update.sh

--- install/update.sh
+++ install/update.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-#source /home/el3ktra/LilL3x/bin/activate
+source /home/el3ktra/LilL3x/bin/activate
 
 sudo rm /var/www/html/*
 sudo ln ~/LilL3x/config/html/* /var/www/html/
 sudo chmod a+x /var/www/html/*.sh
 sudo chown root:root /var/www/html/*.sh
+
+pip install characterai
+pip install PyCharacterAI
