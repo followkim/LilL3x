7790f376b65840013301890ba6ab534389d63bc3
Kimberley Anne Gray <followkim@gmail.com>
2025-02-02 13:54:47 -0800

    Make all directories executeable for website navigation

--- install/update.sh
+++ install/update.sh
@@ -5,7 +5,7 @@ sudo rm /var/www/html/*
 sudo ln ~/LilL3x/config/html/* /var/www/html/
 sudo chmod a+x /var/www/html/*.sh
 sudo chown root:root /var/www/html/*.sh
+sudo chmod a+x /home/el3ktra ~/LilL3x/config  ~/LilL3x/training  ~/LilL3x/picts  ~/LilL3x/log
 
-pip install characterai
-pip install PyCharacterAI
-pip install openai -U
+
+
