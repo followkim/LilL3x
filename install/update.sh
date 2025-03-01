4c372cd13cd24f2f079a1c2029fbf23f7593895a
Kimberley Anne Gray <followkim@gmail.com>
2025-02-28 22:06:41 -0800

    create vosk directory, just in case

--- install/update.sh
+++ install/update.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 source /home/el3ktra/LilL3x/bin/activate
 
+mkdir vosk
 cd vosk
 wget https://alphacephei.com/vosk/models/vosk-model-small-en-us-0.15.zip
 unzip vosk-model-small-en-us-0.15.zip
