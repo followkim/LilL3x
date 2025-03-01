5224b870a9ad9e8fce2733133aa800e6b695b7eb
Kimberley Anne Gray <followkim@gmail.com>
2025-02-28 22:02:35 -0800

    download vosk model, store in vosk folder

--- install/update.sh
+++ install/update.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 source /home/el3ktra/LilL3x/bin/activate
 
-./error_handling.py config/config.txt
-
+cd vosk
+wget https://alphacephei.com/vosk/models/vosk-model-small-en-us-0.15.zip
+unzip vosk-model-small-en-us-0.15.zip
+cd ..
+ln -s vosk model
 
