5ddf4ec3f27d37075c4abea28698b4c28c5b2540
Kimberley Anne Gray <followkim@gmail.com>
2025-09-11 19:01:27 -0700

    Check microphone and set device prior to launching program.

---
+++ mic.py
@@ -0,0 +1,20 @@
+import speech_recognition as sr
+
+# Replace 'YOUR_DEVICE_INDEX' with the actual index obtained from step 1
+device_index = 0
+
+r = sr.Recognizer()
+r.energy_threshold = 1000
+r.pause_threshold = 0.1
+r.phrase_time_limit =1
+
+with sr.Microphone(device_index=device_index) as source:
+    print("Say something!")
+    audio = r.listen(source)
+
+try:
+    print("You said: " + r.recognize_google(audio))
+except sr.UnknownValueError:
+    print("Speech Recognition could not understand audio")
+except sr.RequestError as e:
+    print(f"Could not request results from Google Speech Recognition service; {e}")
