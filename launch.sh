ffc658558be5ba170a3544f18b9b189d0c4c30c1
Kimberley Anne Gray <followkim@gmail.com>
2025-09-16 19:08:01 -0700

    Skip trying to set the default audio device -- not tested on enough machines

--- launch.sh
+++ launch.sh
@@ -16,7 +16,7 @@ git pull >> ./log/git_$(date +"%Y-%m-%d").txt
 echo "Run update.sh..."
 . $HOME/LilL3x/install/update.sh >> ./log/update_$(date +"%Y-%m-%d").txt
 
-pactl set-default-sink 80
+#pactl set-default-sink 80
 
 echo "set alsactl..."
 /usr/sbin/alsactl --file config/alsasound.state restore
