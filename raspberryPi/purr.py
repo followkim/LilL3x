eda64539cbf1244760120089d597dd16db2ed337
followkim <followkim@gmail.com>
2025-10-12 11:07:30 -0700

    Don't purr if CPU Is hot.  Purr on any fsr touch

--- raspberryPi/purr.py
+++ raspberryPi/purr.py
@@ -25,7 +25,6 @@ class Purr:
     purr_thread = False
     is_purring = False
     audio = False
-    face = False
     fsr1 = 22
     fsr2 = 23
     def __init__(self):
@@ -37,22 +36,22 @@ class Purr:
         led_pin = 13
         self.motor = PWMLED(led_pin)
 
-    def PurrThread(self, audio, face):
+    def PurrThread(self, audio):
         startPet = False
         lastPet = False
         self.audio = audio
-        self.face = face
         LogInfo("PurrThread started")
         while not self.should_quit:
 
-            if GPIO.input(self.fsr1) or GPIO.input(self.fsr2):
+            if (GPIO.input(self.fsr1) or GPIO.input(self.fsr2)):
                 waitFsr = self.fsr1 if GPIO.input(self.fsr2) else self.fsr2
                 is_petting = True
                 LogDebug("Petting Detected")
                 startPet = datetime.now()
                 lastPet = startPet
                 while is_petting == True:
-                    while not GPIO.input(waitFsr) and is_petting and not self.should_quit:
+#                    while not GPIO.input(waitFsr) and is_petting and not self.should_quit:
+                    while not (GPIO.input(self.fsr1) or GPIO.input(self.fsr2)) and is_petting and not self.should_quit:
                         if lastPet + timedelta(seconds=cf.g('STROKE_TO')) < datetime.now() or self.should_quit:
                             is_petting = False
                             self.is_purring = False
@@ -62,14 +61,17 @@ class Purr:
                     if not is_petting: break
                     waitFsr = self.fsr1 if GPIO.input(self.fsr2) else self.fsr2
                     lastPet = datetime.now()
-                    if startPet + timedelta(seconds=cf.g('PURR_SEC')) < datetime.now():
-                        self.purr_motor()
-            else: sleep(cf.g('PURR_SLEEP'))
+                    self.purr_motor()
+
+#                    if startPet + timedelta(seconds=cf.g('PURR_SEC')) < datetime.now():
+#                        self.purr_motor()
+#            else: sleep(cf.g('PURR_SLEEP'))
+            else: sleep(0.5)
         LogInfo("PurrThread exit.")
 
     def purr_motor(self):
         self.is_purring = True
-        if not self.Purring():
+        if not self.Purring() and STATE.temp < cf.g('CPU_MAX_TEMP'):
             self.purr_thread = threading.Thread(target=self.purr_motor_thread, daemon=True)
             self.purr_thread.name = f"{GetHostname()} PurrMotorThread"
             self.purr_thread.start()
@@ -81,35 +83,37 @@ class Purr:
         # play purring
         startPurr = datetime.now()
         purr_channel = self.audio.PlaySound(cf.g('PURR_MP3'), asyn=True, loop=True)
-        self.audio.SetChannelVolume(purr_channel, 0)
+        self.audio.SetChannelVolume(purr_channel, 1)
 
         # Define sine wave parameters
-        frequency = 0.25  # Hz (determines how fast the sine wave cycles)
-        amplitude = 0.5  # Controls the range of brightness (0 to 1)
-        offset = 0.5     # Shifts the sine wave up to ensure values are positive (0 to 1)
+        frequency = 5  # Hz (determines how fast the sine wave cycles)
+        amplitude = 1  # Controls the range of brightness (0 to 1)
+        offset = 0 # 0.5 # Shifts the sine wave up to ensure values are positive (0 to 1)
+        brightness = 0
 
         # Loop to continuously update LED brightness
         t = 0
-        while self.is_purring and not self.should_quit:
-            volume = (min((datetime.now() - startPurr).total_seconds() / (10 * 60), 1) * 0.50) + 0.50
-            self.audio.SetChannelVolume(purr_channel, volume)
+        while self.is_purring and not self.should_quit and STATE.temp < cf.g('CPU_MAX_TEMP'):
+#            volume = (min((datetime.now() - startPurr).total_seconds() / (10 * 60), 1) * 1) + 0
+#            self.audio.SetChannelVolume(purr_channel, volume)
             # Calculate sine wave value (scaled and offset for brightness)
-            brightness = amplitude * sin(2 * pi * frequency * t) + offset
+            brightness = abs(amplitude * sin(2 * pi * frequency * t) + offset)
             t += 0.01  # Adjust for smoother or faster animation
 
             # Ensure brightness stays within valid range (0 to 1)
-            brightness = max(0, min(1, brightness))
-            self.motor.value = (brightness)/2 + 0.5 # range 0.5-1 to always have some power
-
-            if startPurr + timedelta(seconds=cf.g('PURR_SEC')) < datetime.now(): self.face.loving()
+            brightness = (max(0, min(1, brightness)) / 4) + 0.75
+            self.motor.value = brightness
 
             # Increment time and control update speed
-            sleep(0.25) # Small sleep to allow the Pi to perform other tasks
+            sleep(0.1) # Small sleep to allow the Pi to perform other tasks
 
 
-        self.motor.off() # Turn off the LED on exit
         self.audio.StopChannel(purr_channel, cf.g("PURR_FO"))
-        self.face.off()
+        while (brightness > 0): # fade out purring motor
+            brightness -= 0.05
+            self.motor.value = max(brightness, 0)
+            sleep(0.1)
+        self.motor.off() # Turn off the purring on exit
 
     def Close(self):
         self.should_quit = True
@@ -124,6 +128,8 @@ if __name__ == '__main__':
             return False
         def SetChannelVolume(self, c, v):
             pass
+        def StopChannel(self, c, v):
+            pass
     a = A()
 
     class B:
@@ -134,8 +140,8 @@ if __name__ == '__main__':
     b = B()
 
     p = Purr()
-    p.PurrThread(a, b)
-#    p.audio = a
+    p.PurrThread(a)
+    p.audio = a
 #    p.face = b
-#    p.is_purring = True
-#    p.purr_motor_thread()
+    p.is_purring = True
+    p.purr_motor_thread()
