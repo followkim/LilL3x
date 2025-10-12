1fca4212271e32b6d54d68a0e92422054a5e9255
followkim <followkim@gmail.com>
2025-10-12 11:11:10 -0700

    Show eyes as thinking while loading

--- launch.sh
+++ launch.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 # launcher.sh
 # navigate to home directory, then to this directory, then execute python script, then back home
@@ -6,6 +7,7 @@ source $HOME/.bash_profile
 date
 
 cd $HOME/LilL3x
+sudo python face.py &
 
 echo "git pull..."
 git pull >> ./log/git_$(date +"%Y-%m-%d").txt
@@ -13,24 +15,16 @@ git pull >> ./log/git_$(date +"%Y-%m-%d").txt
 echo "Run update.sh..."
 . $HOME/LilL3x/install/update.sh >> ./log/update_$(date +"%Y-%m-%d").txt
 
-echo "Checking sound card..."
-l=$(eval "pactl info | grep 'Default Sink' | cut -d ':' -f 2 | xargs")
-while [[ -z "$l" ]]; do
-  echo "No default audio card.  Waiting."
-  l=$(eval "pactl info | grep 'Default Sink' | cut -d ':' -f 2 | xargs")
-  sleep 5
-done
-echo "Default Sound Card loaded"
-pactl info | grep Sink
-
-
-echo "set alsactl..."
-/usr/sbin/alsactl --file config/alsasound.state restore
-
 echo preload mic
 /home/el3ktra/LilL3x/bin/python mic.py
 
+#echo "set alsactl..."
+sudo /usr/sbin/alsactl --file config/alsasound.state restore
+
 echo "Starting LilL3x..."
+#mpg123 media/startup.mp3
+sudo rm ".running"
+sleep 1
 sudo /home/el3ktra/LilL3x/bin/python lillex.py >> ./log/launch_$(date +"%Y-%m-%d").txt 2>&1
 
 echo "Done."
