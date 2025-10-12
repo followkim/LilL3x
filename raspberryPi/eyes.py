593353bd1d1ba235196f226547d40f33b192bccb
followkim <followkim@gmail.com>
2025-10-12 11:06:53 -0700

    Changed number of LEDS for larger cat

--- raspberryPi/eyes.py
+++ raspberryPi/eyes.py
@@ -13,7 +13,7 @@ import neopixel
 pixel_pin = board.D12
 
 # The number of NeoPixels
-num_pixels = 16
+num_pixels = 48
 
 # The order of the pixel colors - RGB or GRB. Some NeoPixels have red and green reversed!
 # For RGBW NeoPixels, simply change the ORDER to RGBW or GRBW.
