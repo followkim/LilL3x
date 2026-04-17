1ca5af929f8a374941654c353d1f29ad68f91a4c
Kimberley Gray <followkim@gmail.com>
2026-04-16 22:10:57 -0700

    Update config_tools.php include path

--- config/index.php
+++ config/index.php
@@ -3,7 +3,7 @@
 	// turn on error reporting
 	error_reporting(E_ALL ^ E_NOTICE);
 	ini_set('display_errors', true); 
-        include '/home/el3ktra/LilL3x/config/config_tools.php';
+        include  __DIR__ . '/config_tools.php';
 
         echo"<html>";
 	HTMLHead();
