f7a8055290b47dc61fbd8c9aac980e9bfd318f21
Kimberley Anne Gray <followkim@gmail.com>
2026-04-19 11:40:05 -0700

    File reorg, removed interactive elements

--- install/install.sh
+++ install/install.sh
@@ -1,17 +1,13 @@
+#I don't recommend trying to run this script.  Rather, run smaller chunks one at a time to monitor that everything is going well.
 sudo apt-get update
 
 git clone -b v2 http://github.com/followkim/LilL3x/
 python -m venv --system-site-packages LilL3x
-  ## TODO: take action if this line isn't there
 source LilL3x/bin/activate
 echo "source LilL3x/bin/activate" | sudo tee -a ~/.bashrc
 echo "cd ~/LilL3x/" | sudo tee -a ~/.bashrc
 
 echo "$USER ALL=(ALL) NOPASSWD: ALL" | sudo tee -a /etc/sudoers
-
-cd ~/LilL3x/
-
-sudo apt-get update
 
 sudo apt install -y python3-picamera2
 sudo apt-get install -y python3-pil
@@ -43,8 +39,6 @@ pip install picamera2
 pip install deepface
 pip install tf-keras
 
-##pip install libcamera
-
 pip install llamaapi
 pip install ollama
 pip install langchain_ollama
@@ -57,16 +51,6 @@ pip install characterai
 #Speech regognition
 pip install git+https://github.com/followkim/speech_recognition.git
 
-# seeed voicecard
-cd ~
-uname -rn  # get the kernal version, should be 6.6
-git clone -b v6.12 https://github.com/HinTak/seeed-voicecard/
-cd seeed-voicecard/
-sudo ./install.sh
-sudo raspi-config #TODO automate: pick sound card, turn on i2c 
-# test 
-arecord -D "plughw:3,0" -f S16_LE -r 16000 -d 2 -t wav test.wav;aplay -D "plughw:3,0" test.wav
-
 # screen
 # GPIO pins: SDA: 3, SCL 5
 cd ~
@@ -75,12 +59,59 @@ wget https://raw.githubusercontent.com/adafruit/Raspberry-Pi-Installer-Scripts/m
 sudo -E env PATH=$PATH python3 raspi-blinka.py
 pip install adafruit-circuitpython-ssd1306
 
+
+# seeed voicecard
+cd ~
+kernal_num=$(uname -r | grep -o '[0-9]\.[0-9]*')
+git clone -b v$kernal_num https://github.com/HinTak/seeed-voicecard/
+cd seeed-voicecard/
+sudo ./install.sh
+#sudo raspi-config #TODO automate: pick sound card, turn on i2c 
+# test 
+#read -p "Speak to test the voice card.  Press enter when ready."
+#arecord -D "plughw:3,0" -f S16_LE -r 16000 -d 2 -t wav test.wav;aplay -D "plughw:3,0" test.wav
+
+# get default wakewords -- #TODO: just grab directory not entire repo!!
+cd ~
+git clone http://github.com/Picovoice/porcupine.git
+cp porcupine/resources/keyword_files/raspberry-pi/* ~/LilL3x/wake
+rm -rf porcupine
+
+cd vosk
+wget https://alphacephei.com/vosk/models/vosk-model-small-en-us-0.15.zip
+unzip vosk-model-small-en-us-0.15.zip
+
+
 # system changes
 (crontab -l; echo "@reboot sh /home/$USER/LilL3x/launch.sh >> /home/$USER/LilL3x/log/cronlog") | crontab -
 
-#echo "Enter a password (8 letters): "
-#read password
 
+cp /etc/skel/.bashrc ~/.bashrc
+cat install/bashrc >> ~/.bashrc
+chmod +x ~/.bashrc
+
+# install website
+sudo apt-get install -y apache2
+sudo apt-get install -y php
+sudo rm /etc/apache2/sites-available/000-default.conf
+sudo cp -l ~/LilL3x/install/apache_default.conf /etc/apache2/sites-available/lill3x.conf
+sudo chown root:root /etc/apache2/sites-available/lill3x.conf
+sudo chmod a+rwx /etc/apache2/sites-available/lill3x.conf
+sudo sed -i "s/USER/$USER/g" /etc/apache2/sites-available/lill3x.conf
+sudo a2ensite lill3x.conf
+sudo chmod a+rx ~  # needed to be able to browse folders in home folder
+sudo systemctl restart apache2
+
+# make sure the website can read the file
+cp ~/LilL3x/config/config.default ~/LilL3x/config/config.txt
+sudo usermod -a -G www-data $USER
+sudo usermod -a -G www-data www-data
+sudo chown $USER:www-data ~/LilL3x/config ~/LilL3x/config/config.txt
+sudo chmod ug+rw  ~/LilL3x/config ~/LilL3x/config/config.txt
+
+
+
+# Install the backup network
 sudo nmcli connection add \
  type wifi \
  con-name $HOSTNAME \
@@ -93,57 +124,21 @@ sudo nmcli connection add \
  ipv4.method shared \
  ipv6.method shared
 
-
-
-sudo cp ~/LilL3x/install/rclocal /etc/rc.local # if no rc.local
-#sudo sed -i "s/PASSWORD/$password/g" /etc/rc.local
-sudo chown root:root /etc/rc.local
-sudo chmod +x /etc/rc.local
-
-cp /etc/skel/.bashrc ~/.bashrc
-cat install/bashrc >> ~/.bashrc
-chmod +x ~/.bashrc
-sudo chmod +x /etc/rc.local
-
-# install website -- TODO set base folder
-sudo apt-get install -y apache2
-sudo apt-get install -y php
-sudo rm /var/www/html/*
-sudo ln ~/LilL3x/config/html/* /var/www/html/
-cp ~/LilL3x/config/config.default ~/LilL3x/config/config.txt
-
-sudo usermod -a -G www-data $USER
-sudo usermod -a -G www-data www-data
-
-sudo chmod a+x /var/www/html/*.sh
-sudo chown root:root /var/www/html/*.sh
-sudo chown $USER:www-data ~/LilL3x/config ~/LilL3x/config/config.txt
-sudo chmod ug+rw  ~/LilL3x/config ~/LilL3x/config/config.txt
-echo "%www-data ALL=NOPASSWD: /var/www/html/listwifi.sh" | sudo tee -a /etc/sudoers
-echo "%www-data ALL=NOPASSWD: /var/www/html/setwifi.sh" | sudo tee -a /etc/sudoers
- #use dedicated sudoers.d file
+#use dedicated sudoers.d file
 echo "%www-data ALL=NOPASSWD: /home/$USER/LilL3x/config/html/listwifi.sh" | sudo tee /etc/sudoers.d/lill3x
 echo "%www-data ALL=NOPASSWD: /home/$USER/LilL3x/config/html/setwifi.sh" | sudo tee -a /etc/sudoers.d/lill3x
 
-#sudo rm /etc/apache2/sites-available/000-default.conf
-sudo cp -l ~/LilL3x/install/apache_default.conf /etc/apache2/sites-available/lill3x.conf
-sudo chown root:root /etc/apache2/sites-available/lill3x.conf
-sudo chmod a+rwx /etc/apache2/sites-available/lill3x.conf
-sed "s/USER/$USER/g" /etc/apache2/sites-available/lill3x.conf
-sudo a2ensite lill3x.conf
-sudo chmod a+rx ~
-sudo systemctl restart apache2
 
-# get default wakewords -- #TODO: just grab directory not entire repo!!
-cd ~
-git clone http://github.com/Picovoice/porcupine.git
-cp porcupine/resources/keyword_files/raspberry-pi/* ~/LilL3x/wake
-rm -rf porcupine
+sudo cp ~/LilL3x/install/rclocal /etc/rc.local # if no rc.local
+sudo chown root:root /etc/rc.local
+sudo chmod +x /etc/rc.local
 
-cd ~/LilL3x/
+sudo rm /var/www/html/*
+sudo ln ~/LilL3x/config/html/* /var/www/html/
 
-cd vosk
-wget https://alphacephei.com/vosk/models/vosk-model-small-en-us-0.15.zip
-unzip vosk-model-small-en-us-0.15.zip
+sudo chmod a+x /var/www/html/*.sh
+sudo chown root:root /var/www/html/*.sh
+
+
 
 cd ~/LilL3x/
