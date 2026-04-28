64656b46160e427e784ef6caef7a245c485fa9c7
Kimberley Gray <followkim@gmail.com>
2026-04-27 19:24:31 -0700

    Update package installations in update.sh
    Removed installation of pyimgur and google.genai. Updated openwakeword installation and added typecast-python upgrade.

--- install/update.sh
+++ install/update.sh
@@ -2,14 +2,5 @@
 
 source $HOME/LilL3x/bin/activate
 
-pip install pyimgur
-pip install google.genai
 
-pip uninstall -y openwakeword
-pip install openwakeword --no-deps
-pip install onnxruntime numpy tqdm scipy requests scikit-learn
-python -c "import openwakeword; openwakeword.utils.download_models()"
-ln $HOME/LilL3x/lib/python3.13/site-packages/openwakeword/resources/models/*.onnx $HOME/LilL3x/wake
-
-#pip install gpiozero
-
+pip install --upgrade typecast-python
