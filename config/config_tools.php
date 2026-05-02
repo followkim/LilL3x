e3fefc3d57f17198210bf5cac2b24ae32c7ed284
Kimberley Gray <followkim@gmail.com>
2026-05-02 09:19:59 -0700

    Added CSS page to config

--- config/config_tools.php
+++ config/config_tools.php
@@ -16,6 +16,7 @@
 	  echo "<head>";
 	  echo " <title>".gethostname()."</title>";
 	  echo '  <meta name="viewport" content="width=device-width, initial-scale=1">';
+          echo '  <link rel="stylesheet" href="'.'html/lill3x.css'.'">';
 	  echo "</head>";
 	}
 
